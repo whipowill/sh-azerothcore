72615297e8ea05961e136f92d1e094aa4581de2c
whipowill <whipowill@protonmail.com>
2022-05-18 09:08:28 -0500

    Update.

--- actions/modules.sh
+++ actions/modules.sh
@@ -7,8 +7,8 @@ rm -rf mod-*
 # install modules (you pick what you want)
 git clone https://github.com/azerothcore/mod-autobalance.git
 #git clone https://github.com/azerothcore/mod-ah-bot.git
-#git clone https://github.com/azerothcore/mod-account-achievements.git FATAL ERROR NO MATTER WHAT
-git clone https://github.com/ZhengPeiRu21/mod-playerbots.git
+#git clone https://github.com/azerothcore/mod-account-achievements.git # FATAL ERROR NO MATTER WHAT
+#git clone https://github.com/ZhengPeiRu21/mod-playerbots.git # ONLY USE W/ https://github.com/ZhengPeiRu21/azerothcore-wotlk.git
 #git clone https://github.com/benjymansy123/mod-worgoblin.git
 #git clone https://github.com/azerothcore/mod-bg-twinpeaks.git
 #git clone https://github.com/azerothcore/mod-bg-battle-for-gilneas.git
