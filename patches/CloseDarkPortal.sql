944bc325871b0a3f35e8c694fafbe83217454260
whipowill <whipowill@protonmail.com>
2023-08-01 23:23:26 -0600

    Updates.

---
+++ patches/CloseDarkPortal.sql
@@ -0,0 +1,3 @@
+# disable Dark Portal
+UPDATE areatrigger_teleport SET target_map=0, target_position_x=-11883.2, target_position_y=-3206.1, target_position_z=-16.616, target_orientation=0.1357 WHERE ID=4354;
+# https://github.com/azerothcore/mod-progression-system/pull/279/commits/64d4653058273031e1ac7ac6d05e19c5df114941
