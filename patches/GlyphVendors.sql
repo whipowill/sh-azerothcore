bb2b1611f12a498bfd171dd68b0d1baeca636402
whipowill <whipowill@protonmail.com>
2023-08-20 17:10:33 -0600

    Updates.

--- patches/GlyphVendors.sql
+++ patches/GlyphVendors.sql
@@ -7,9 +7,9 @@ the "Inkwell Cartel" to the auction area in Gadgetzan.
 -- Glyph Vendor - DK
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200000;
-DELETE FROM creature WHERE id1>=200000;
-DELETE FROM npc_vendor WHERE entry>=200000;
+DELETE FROM creature_template WHERE entry=200000;
+DELETE FROM creature WHERE id1=200000;
+DELETE FROM npc_vendor WHERE entry=200000;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Tinker Tosslecrank", "Inkwell Cartel", 40, 40, 475, 4225, 200000, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200000, 43533);
@@ -50,9 +50,9 @@ INSERT INTO npc_vendor (entry, item) VALUES (200000, 45806);
 -- Glyph Vendor - Druid
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200001;
-DELETE FROM creature WHERE id1>=200001;
-DELETE FROM npc_vendor WHERE entry>=200001;
+DELETE FROM creature_template WHERE entry=200001;
+DELETE FROM creature WHERE id1=200001;
+DELETE FROM npc_vendor WHERE entry=200001;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Klink Goldsprocket", "Inkwell Cartel", 40, 40, 475, 4225, 200001, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200001, 40896);
@@ -98,9 +98,9 @@ INSERT INTO npc_vendor (entry, item) VALUES (200001, 50125);
 -- Glyph Vendor - Hunter
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200002;
-DELETE FROM creature WHERE id1>=200002;
-DELETE FROM npc_vendor WHERE entry>=200002;
+DELETE FROM creature_template WHERE entry=200002;
+DELETE FROM creature WHERE id1=200002;
+DELETE FROM npc_vendor WHERE entry=200002;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Snickers Blastwhiz", "Inkwell Cartel", 40, 40, 475, 4225, 200002, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200002, 42897);
@@ -141,9 +141,9 @@ INSERT INTO npc_vendor (entry, item) VALUES (200002, 45735);
 -- Glyph Vendor - Mage
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200003;
-DELETE FROM creature WHERE id1>=200003;
-DELETE FROM npc_vendor WHERE entry>=200003;
+DELETE FROM creature_template WHERE entry=200003;
+DELETE FROM creature WHERE id1=200003;
+DELETE FROM npc_vendor WHERE entry=200003;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Fizzle Whizzblast", "Inkwell Cartel", 40, 40, 475, 4225, 200003, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200003, 42734);
@@ -187,9 +187,9 @@ INSERT INTO npc_vendor (entry, item) VALUES (200003, 50045);
 -- Glyph Vendor - Paladin
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200004;
-DELETE FROM creature WHERE id1>=200004;
-DELETE FROM npc_vendor WHERE entry>=200004;
+DELETE FROM creature_template WHERE entry=200004;
+DELETE FROM creature WHERE id1=200004;
+DELETE FROM npc_vendor WHERE entry=200004;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Ratchet Geargrin", "Inkwell Cartel", 40, 40, 475, 4225, 200004, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200004, 41092);
@@ -231,9 +231,9 @@ INSERT INTO npc_vendor (entry, item) VALUES (200004, 45747);
 -- Glyph Vendor - Rogue
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200005;
-DELETE FROM creature WHERE id1>=200005;
-DELETE FROM npc_vendor WHERE entry>=200005;
+DELETE FROM creature_template WHERE entry=200005;
+DELETE FROM creature WHERE id1=200005;
+DELETE FROM npc_vendor WHERE entry=200005;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Bling Blingerson", "Inkwell Cartel", 40, 40, 475, 4225, 200005, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200005, 42954);
@@ -275,9 +275,9 @@ INSERT INTO npc_vendor (entry, item) VALUES (200005, 45769);
 -- Glyph Vendor - Shaman
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200006;
-DELETE FROM creature WHERE id1>=200006;
-DELETE FROM npc_vendor WHERE entry>=200006;
+DELETE FROM creature_template WHERE entry=200006;
+DELETE FROM creature WHERE id1=200006;
+DELETE FROM npc_vendor WHERE entry=200006;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Sparky Boomflame", "Inkwell Cartel", 40, 40, 475, 4225, 200006, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200006, 41517);
@@ -320,9 +320,9 @@ INSERT INTO npc_vendor (entry, item) VALUES (200006, 45778);
 -- Glyph Vendor - Warlock
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200007;
-DELETE FROM creature WHERE id1>=200007;
-DELETE FROM npc_vendor WHERE entry>=200007;
+DELETE FROM creature_template WHERE entry=200007;
+DELETE FROM creature WHERE id1=200007;
+DELETE FROM npc_vendor WHERE entry=200007;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Razzle Quickfuse", "Inkwell Cartel", 40, 40, 475, 4225, 200007, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200007, 42453);
@@ -365,9 +365,9 @@ INSERT INTO npc_vendor (entry, item) VALUES (200007, 50077);
 -- Glyph Vendor - Warrior
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200008;
-DELETE FROM creature WHERE id1>=200008;
-DELETE FROM npc_vendor WHERE entry>=200008;
+DELETE FROM creature_template WHERE entry=200008;
+DELETE FROM creature WHERE id1=200008;
+DELETE FROM npc_vendor WHERE entry=200008;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Gizmo Gearspark", "Inkwell Cartel", 40, 40, 475, 4225, 200008, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200008, 43395);
@@ -409,9 +409,9 @@ INSERT INTO npc_vendor (entry, item) VALUES (200008, 49084);
 -- Glyph Vendor - Priest
 -- =================================================
 
-DELETE FROM creature_template WHERE entry>=200009;
-DELETE FROM creature WHERE id1>=200009;
-DELETE FROM npc_vendor WHERE entry>=200009;
+DELETE FROM creature_template WHERE entry=200009;
+DELETE FROM creature WHERE id1=200009;
+DELETE FROM npc_vendor WHERE entry=200009;
 
 INSERT INTO creature_template (modelid1, name, subname, minlevel, maxlevel, faction, npcflag, entry, speed_walk, speed_run, scale, DamageModifier, unit_class, HealthModifier, ManaModifier, ArmorModifier, RegenHealth, VerifiedBuild) VALUES (7164, "Zipp Gearwrench", "Inkwell Cartel", 40, 40, 475, 4225, 200009, 1, 1.14286, 1, 1, 1, 1, 1, 1, 1, 1);
 INSERT INTO npc_vendor (entry, item) VALUES (200009, 42396);
@@ -467,4 +467,3 @@ SET BuyPrice = (RequiredLevel / 2) * 100 * 100
 WHERE entry IN (
 '43533', '43534', '43535', '43536', '43537', '43538', '43539', '43541', '43542', '43543', '43544', '43545', '43546', '43547', '43548', '43549', '43550', '43551', '43552', '43553', '43554', '43671', '43672', '43673', '43825', '43826', '43827', '45799', '45800', '45803', '45804', '45805', '45806', '40896', '40897', '40899', '40900', '40901', '40902', '40903', '40906', '40908', '40909', '40912', '40913', '40914', '40915', '40916', '40919', '40920', '40921', '40922', '40923', '40924', '43316', '43331', '43332', '43334', '43335', '43674', '44922', '44928', '45601', '45602', '45603', '45604', '45622', '45623', '46372', '48720', '50125', '42897', '42898', '42899', '42900', '42901', '42902', '42903', '42904', '42905', '42906', '42907', '42908', '42909', '42910', '42911', '42912', '42913', '42914', '42915', '42916', '42917', '43338', '43350', '43351', '43354', '43355', '43356', '45625', '45731', '45732', '45733', '45734', '45735', '42734', '42735', '42736', '42737', '42738', '42739', '42740', '42741', '42742', '42743', '42744', '42745', '42746', '42747', '42748', '42749', '42750', '42751', '42752', '42753', '42754', '43339', '43357', '43359', '43360', '43361', '43364', '44684', '44920', '44955', '45736', '45737', '45738', '45739', '45740', '50045', '41092', '41094', '41095', '41096', '41097', '41098', '41099', '41100', '41101', '41102', '41103', '41104', '41105', '41106', '41107', '41108', '41109', '41110', '43340', '43365', '43366', '43367', '43368', '43369', '43867', '43868', '43869', '45741', '45742', '45743', '45744', '45745', '45746', '45747', '42954', '42955', '42956', '42957', '42958', '42959', '42960', '42961', '42962', '42963', '42964', '42965', '42966', '42967', '42968', '42969', '42970', '42971', '42972', '42973', '42974', '43343', '43376', '43377', '43378', '43379', '43380', '45761', '45762', '45764', '45766', '45767', '45768', '45769', '41517', '41518', '41524', '41526', '41527', '41529', '41530', '41531', '41532', '41533', '41534', '41535', '41536', '41537', '41538', '41539', '41540', '41541', '41542', '41547', '41552', '43344', '43381', '43385', '43386', '43388', '43725', '44923', '45770', '45771', '45772', '45775', '45776', '45777', '45778', '42453', '42454', '42455', '42456', '42457', '42458', '42459', '42460', '42461', '42462', '42463', '42464', '42465', '42466', '42467', '42468', '42469', '42470', '42471', '42472', '42473', '43389', '43390', '43391', '43392', '43393', '43394', '45779', '45780', '45781', '45782', '45783', '45785', '45789', '50077', '43395', '43396', '43397', '43398', '43399', '43400', '43412', '43413', '43414', '43415', '43416', '43417', '43418', '43419', '43420', '43421', '43422', '43423', '43424', '43425', '43426', '43427', '43428', '43429', '43430', '43431', '43432', '45790', '45792', '45793', '45794', '45795', '45797', '49084', '42396', '42397', '42398', '42399', '42400', '42401', '42402', '42403', '42404', '42405', '42406', '42407', '42408', '42409', '42410', '42411', '42412', '42414', '42415', '42416', '42417', '43342', '43370', '43371', '43372', '43373', '43374', '45753', '45755', '45756', '45757', '45758', '45760'
 );
-
