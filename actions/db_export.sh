c1cd57959e61d2768e338614a3435e9bb0e0ac03
whipowill <whipowill@protonmail.com>
2023-12-09 20:56:17 -0700

    Bug fixes.

--- actions/db_export.sh
+++ actions/db_export.sh
@@ -1,3 +1,3 @@
 mkdir ~/backups/db
-mysqldump --user=root --password=root acore_accounts > ~/backups/db/acore_accounts.sql
+mysqldump --user=root --password=root acore_auth > ~/backups/db/acore_accounts.sql
 mysqldump --user=root --password=root acore_characters > ~/backups/db/acore_characters.sql
